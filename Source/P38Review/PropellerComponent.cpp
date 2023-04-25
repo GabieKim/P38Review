c006db1b8d1ef238631b8f58e944ee18fbc7a965
GabieKim <rumisonic@gmail.com>
2023-04-25 14:56:21 +0900

    6. 로켓 컴포넌트 달고 메쉬 넣어줌

---
+++ Source/P38Review/PropellerComponent.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PropellerComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+UPropellerComponent::UPropellerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UPropellerComponent::TickComponent(float DeltaTime, ELevelTick TickType, 
+	FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	AddLocalRotation(FRotator(0, 
+		0, 
+		3600.0f * UGameplayStatics::GetWorldDeltaSeconds(GetWorld())
+	));
+}
+
+
---
+++ Source/P38Review/Rocket.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Rocket.generated.h"
+
+UCLASS()
+class P38REVIEW_API ARocket : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ARocket();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class UBoxComponent* Box;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class UStaticMeshComponent* Body;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class UProjectileMovementComponent* Movement;
+};
